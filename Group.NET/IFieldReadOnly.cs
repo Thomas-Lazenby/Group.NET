99281082a679ffc1f544e6855b608d96911467fa
Thomas Lazenby <t1758219@gmail.com>
2024-11-26 18:52:32 +0000

    Added IField and IFieldReadONly

---
+++ Group.NET/IFieldReadOnly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group.NET
+{
+
+    public interface IFieldReadOnly<TKey, TValue>
+        where TKey : IEquatable<TKey>
+    {
+
+        /// <summary> Gets all fields. </summary>
+        IEnumerable<TKey> GetKeysField();
+
+        T GetField<T>(TKey key)
+            where T : TValue;
+
+
+        public bool TryGetField<T>(TKey key, out T? value)
+            where T : TValue;
+
+        bool ExistsField(TKey key);
+    }
+}
